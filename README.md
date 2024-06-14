a5f0c00d3dfc30f6de7cbf464ef5e15b5e2be009
Swapnoneel Saha <swapnoneelsaha111@gmail.com>
2024-06-14 17:40:25 +0530

    v1.0.0

---
+++ README.md
@@ -0,0 +1,25 @@
+# Generate Code from UI
+
+This Streamlit app is designed to convert UI designs into code using the power of AI. It analyzes uploaded images of UI designs and generates corresponding HTML code, making it easier for developers to bring their designs to life.
+
+## Installation
+
+To install the necessary dependencies, run the following command:
+
+```
+pip install -r requirements.txt
+```
+
+## Running the App
+
+To run the app, use the following command:
+
+```
+python -m streamlit run app.py
+```
+
+## Configuration
+
+Before running the app, you may configure your own API key in the `app.py` file. Replace the current API key with your API key and also configure the model name to best fit your results with your expectations.
+
+You can also tell Gemini to use a specific CSS framework, like Bootstrap or Tailwind CSS!
---
+++ app.py
@@ -0,0 +1,89 @@
+import streamlit as st
+import pathlib
+from PIL import Image
+import google.generativeai as genai
+
+FRAMEWORK = "Regular CSS use flex grid etc"
+MODEL_NAME = "gemini-1.5-flash-latest"
+API_KEY = 'AIzaSyAdc5fab0S7N3gr6aB2AHwjFCuaaR0hHpU'
+genai.configure(api_key=API_KEY)
+
+generation_config = {
+    "temperature": 1,
+    "top_p": 0.95,
+    "top_k": 64,
+    "max_output_tokens": 8192,
+    "response_mime_type": "text/plain",
+}
+
+safety_settings = [
+    {"category": "HARM_CATEGORY_HARASSMENT", "threshold": "BLOCK_NONE"},
+    {"category": "HARM_CATEGORY_HATE_SPEECH", "threshold": "BLOCK_NONE"},
+    {"category": "HARM_CATEGORY_SEXUALLY_EXPLICIT", "threshold": "BLOCK_NONE"},
+    {"category": "HARM_CATEGORY_DANGEROUS_CONTENT", "threshold": "BLOCK_NONE"},
+]
+
+model = genai.GenerativeModel(
+    model_name=MODEL_NAME,
+    safety_settings=safety_settings,
+    generation_config=generation_config,
+)
+
+chat_session = model.start_chat(history=[])
+
+def send_message_to_model(message, image_path):
+    image_input = {
+        'mime_type': 'image/jpeg',
+        'data': pathlib.Path(image_path).read_bytes()
+    }
+    response = chat_session.send_message([message, image_input])
+    return response.text
+
+def main():
+    st.title("Generate Code from UI 👨‍💻 ")
+    st.subheader('Created by [Swapnoneel](https://x.com/swapnoneel123)')
+
+    uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
+
+    if uploaded_file is not None:
+        try:
+            image = Image.open(uploaded_file)
+            st.image(image, caption='Uploaded Image.', use_column_width=True)
+
+            if image.mode == 'RGBA':
+                image = image.convert('RGB')
+
+            temp_image_path = pathlib.Path("temp_image.jpg")
+            image.save(temp_image_path, format="JPEG")
+
+            if st.button("Code UI"):
+                st.write("👀 **_Looking at your UI..._**")
+                prompt = "Describe this UI in accurate details. When you reference a UI element put its name and bounding box in the format: [object name (y_min, x_min, y_max, x_max)]. Also Describe the color of the elements."
+                description = send_message_to_model(prompt, temp_image_path)
+                st.write(description)
+
+                st.write("🔍 **_Refining description with visual comparison..._**")
+                refine_prompt = f"Compare the described UI elements with the provided image and identify any missing elements or inaccuracies. Also Describe the color of the elements. Provide a refined and accurate description of the UI elements based on this comparison. Here is the initial description: {description}"
+                refined_description = send_message_to_model(refine_prompt, temp_image_path)
+                st.write(refined_description)
+
+                st.write("🛠️ **_Generating website..._**")
+                html_prompt = f"Create an HTML file based on the following UI description, using the UI elements described in the previous response. Include {FRAMEWORK} CSS within the HTML file to style the elements. Make sure the colors used are the same as the original UI. The UI needs to be responsive and mobile-first, matching the original UI as closely as possible. Do not include any explanations or comments. Avoid using ```html. and ``` at the end. ONLY return the HTML code with inline CSS. Here is the refined description: {refined_description}"
+                initial_html = send_message_to_model(html_prompt, temp_image_path)
+                st.code(initial_html, language='html')
+
+                st.write("🔧 **_Refining website..._**")
+                refine_html_prompt = f"Validate the following HTML code based on the UI description and image and provide a refined version of the HTML code with {FRAMEWORK} CSS that improves accuracy, responsiveness, and adherence to the original design. ONLY return the refined HTML code with inline CSS. Avoid using ```html. and ``` at the end. Here is the initial HTML: {initial_html}"
+                refined_html = send_message_to_model(refine_html_prompt, temp_image_path)
+                st.code(refined_html, language='html')
+
+                with open("index.html", "w") as file:
+                    file.write(refined_html)
+                st.success("**HTML file 'index.html' has been created.**")
+
+                st.download_button(label="Download HTML", data=refined_html, file_name="index.html", mime="text/html")
+        except Exception as e:
+            st.error(f"An error occurred: {e}")
+
+if __name__ == "__main__":
+    main()
